42c70eff267d5079ab0bf42cfc8a0d6a2339a761
Carles Cruces <japonizante@gmail.com>
2022-02-19 12:37:57 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { getTokenOrRefresh } from './helpers/token_utils';
+import { ResultReason, PronunciationAssessmentConfig } from 'microsoft-cognitiveservices-speech-sdk';
+import { Container } from 'reactstrap';
 
-function App() {
+const speechsdk = require('microsoft-cognitiveservices-speech-sdk');
+
+export const App = () => {
+
+  const [msg, setMsg] = useState('INITIALIZED: ready to speech...');
+
+  const sttFromMic = async () => {
+    const tokenRes = await getTokenOrRefresh();
+    console.log(tokenRes);
+
+    if (tokenRes.authToken === null) {
+      setMsg(`FATAL_ERROR: ${tokenRes.error}`);
+    } else {
+      const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenRes.authToken, tokenRes.region);
+      speechConfig.speechRecognitionLanguage = 'en-US';
+
+      const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+      const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+
+      const pronunciationAssesmentConfig = new PronunciationAssessmentConfig('Hello darling, how are you?',
+        speechsdk.PronunciationAssessmentGradingSystem.HundredMark,
+        speechsdk.PronunciationAssessmentGranularity.Phoneme, true);
+
+      pronunciationAssesmentConfig.applyTo(recognizer);
+
+      setMsg('Speack into your microphone...');
+
+      recognizer.recognizeOnceAsync(result => {
+        let displayText;
+        if (result.reason === ResultReason.RecognizedSpeech) {
+          var pronunciationAssessmentResult = speechsdk.PronunciationAssessmentResult.fromResult(result);
+          var pronunciationScore = pronunciationAssessmentResult.pronunciationScore;
+          var wordLevelResult = pronunciationAssessmentResult.detailResult.Words;
+
+          console.log(pronunciationScore);
+          console.log(wordLevelResult);
+          displayText = `RECOGNIZED: Text=${result.text}`
+        } else {
+          displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+        }
+
+        setMsg(displayText);
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Container className="app-container">
+      <h1 className="display-4 mb-3">Speech sample app</h1>
 
-export default App;
+      <div className="row main-container">
+        <div className="col-6">
+          <i className="fas fa-microphone fa-lg me-2" onClick={() => sttFromMic()}></i>
+          Convert speech to text from your mic.         
+        </div>
+        <div className="col-6 output-display rounded">
+          <code>{msg}</code>
+        </div>
+      </div>
+    </Container>
+  )
+}
---
+++ src/helpers/token_utils.js
@@ -0,0 +1,55 @@
+import Cookie from "universal-cookie";
+
+export const getTokenOrRefresh = async () => {
+    const cookie = new Cookie();
+    const speechToken = cookie.get('speech-token');
+    const speechKey = process.env.REACT_APP_SPEECH_KEY;
+    const speechRegion = process.env.REACT_APP_SPEECH_REGION;   
+
+    let tokenResponse;
+    
+    if( !speechToken ){
+        if (speechKey === 'paste-your-speech-key-here' || speechRegion === 'paste-your-speech-region-here') {
+            tokenResponse = {
+                authToken: null,
+                error: 'You forgot to add your speech key or region to the .env file'
+            };
+        } else {
+            const headers = {
+                'Ocp-Apim-Subscription-Key': speechKey,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            };
+
+            await fetch(`https://${speechRegion}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, {
+                method: "post",
+                headers
+            }).then( response => {
+                if(response.ok) {
+                    return response.text().then(data => {
+                        console.log('Token fetched from API', data);
+                        tokenResponse = {
+                            authToken: data,
+                            region: speechRegion
+                        };
+                    });    
+                } else {
+                    tokenResponse = {
+                        authToken: null,
+                        error: "Network reponse OK but HTTP response not OK"
+                    };
+                }
+            }).catch(err => {
+                tokenResponse = {
+                    authToken: null,
+                    error: err
+                };
+            });
+
+            return tokenResponse;
+        }        
+    } else {
+        console.log('Token fetched from cookie', speechToken);
+        const idx = speechToken.indexOf(":");
+        return { authToken: speechToken.slice(idx + 1), region: speechToken.slice(0, idx) };
+    }
+}
